52df596305b25dee511eb7eaeefb675191c7792e
yaroyan001 <yaroyan001@gmail.com>
2022-09-11 15:21:00 +0900

    [add] implements dapper custom type handler.
    DateTimeOffset, Guid, TimeSpan.

---
+++ Assets/Scripts/Infrastructure/DataSource/Dapper/Sqlite/TypeHandler/SqliteTypeHandler.cs
@@ -0,0 +1,35 @@
+using Dapper;
+using System.Data;
+using System;
+
+namespace Yaroyan.Game.RPG.Infrastructure.DataSource.Dapper.SQLite.TypeHandler
+{
+    /// <summary>
+    /// See link below.
+    /// https://docs.microsoft.com/ja-jp/dotnet/standard/data/sqlite/dapper-limitations
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    abstract class SqliteTypeHandler<T> : SqlMapper.TypeHandler<T>
+    {
+        // Parameters are converted by Microsoft.Data.Sqlite
+        public override void SetValue(IDbDataParameter parameter, T value)
+            => parameter.Value = value;
+    }
+    class DateTimeOffsetHandler : SqliteTypeHandler<DateTimeOffset>
+    {
+        public override DateTimeOffset Parse(object value)
+            => DateTimeOffset.Parse((string)value);
+    }
+
+    class GuidHandler : SqliteTypeHandler<Guid>
+    {
+        public override Guid Parse(object value)
+            => Guid.Parse((string)value);
+    }
+
+    class TimeSpanHandler : SqliteTypeHandler<TimeSpan>
+    {
+        public override TimeSpan Parse(object value)
+            => TimeSpan.Parse((string)value);
+    }
+}
